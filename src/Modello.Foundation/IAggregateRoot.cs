ca6c382782d7c5d0be4f1deafc1e9943e32b7695
gasbrieo <gaberabreu.tech@gmail.com>
2025-03-15 10:31:46 -0300

    Add missing interfaces

---
+++ src/Modello.Foundation/IAggregateRoot.cs
@@ -0,0 +1,3 @@
+namespace Modello.Foundation;
+
+public interface IAggregateRoot;
---
+++ src/Modello.Foundation/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Modello.Foundation;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
